9198c1faef2037df115345d38af6353f8ce09d6a
Arpita Mourya <arpitamourya18@gmail.com>
2026-07-10 22:14:50 +0530

    added new folder

---
+++ Practice/10Jul/lambdaeg.cs
@@ -0,0 +1,16 @@
+
+    /*    Func<int,int> square = x => x*x;
+Console.WriteLine(square(6));
+
+Func<int,int,int> subb = (a,b)=> a-b;
+Console.WriteLine(subb(10,50));
+
+void add(int a,int b)
+{
+    int res = a+b;
+    Console.WriteLine(res);
+}
+
+add(18,45);
+    */
+
